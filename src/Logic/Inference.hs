0e2fdbd3599daaf1ecaef233fb887cbcec96b256
Iain Moncrief <iainmoncrief@10-249-25-76.wireless.oregonstate.edu>
2023-06-07 11:18:23 -0700

    included martins haskell-rules project

--- src/Logic/Inference.hs
+++ src/Logic/Inference.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances, UndecidableInstances #-}
 module Logic.Inference where
 
 import Logic.Rules.NDSM
@@ -7,3 +8,18 @@ import Logic.Rules.TypeGU
 import Logic.Proof
 
 
+
+class Judgement i o => Extractable i o j where
+  compose :: i -> o -> j
+  known :: j -> i
+
+-- instance Extractable i o j => Explain j where
+--   premises j = do
+--     let i = known j
+--     o <- infer i
+--     return (compose i o)
+
+
+
+-- instance Extractable i o j => Explain (i,o,j) where
+--   premises (i,o,j) = infer
